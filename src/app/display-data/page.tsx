6e3d4219a735fe37005db598ecd1259cc26ee67e
Nuiosse11421 <notice.gameacc@gmail.com>
2024-12-14 21:07:25 +0700

    learing mkwithjsx and simple display-data

---
+++ src/app/display-data/page.tsx
@@ -0,0 +1,23 @@
+import '../globals.css'
+
+const user = {
+    name: 'Hedy Lamarr',
+    imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+    imageSize: 90,
+};
+
+export default function display1() {
+    return (
+        <>
+            <h1>{user.name}</h1>
+            <img className='avatar' src={user.imageUrl} 
+            alt={'Photo of '+ user.name} 
+            style={{
+                width: user.imageSize,
+                height: user.imageSize,
+            }}
+            />
+            
+        </>
+    )
+}
