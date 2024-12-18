4417cb41c5f30ef5191d9e93043eb7cad6d367e2
Nuiosse11421 <notice.gameacc@gmail.com>
2024-12-18 16:12:52 +0700

    imoprt export and components

---
+++ src/app/im-ex-compo/components/Profile.tsx
@@ -0,0 +1,7 @@
+export default function Profile() { //export return ในส่วนของ Img ออกไปข้างนอกถ้าหากมีการเรียกใช้งาน
+    return (
+        <img
+            src="https://i.imgur.com/QIrZWGIs.jpg"
+            alt="Alan L. Hart" />
+    )
+}
