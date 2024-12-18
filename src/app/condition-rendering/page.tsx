ed63a0111bfff2169738920cb3309ee83019d2ca
Nuiosse11421 <notice.gameacc@gmail.com>
2024-12-18 16:12:06 +0700

    learn condition rendering

---
+++ src/app/condition-rendering/page.tsx
@@ -0,0 +1,33 @@
+interface ItemProps{
+    name:String,
+    isPacked:boolean,
+}
+function Item({ name, isPacked }:ItemProps) {
+    return (
+      <li className="item">
+        {name} {isPacked && '✅'}
+      </li>
+    );
+  }
+  
+  export default function PackingList() {
+    return (
+      <section>
+        <h1>Sally Ride's Packing List</h1>
+        <ul>
+          <Item
+            isPacked={true}
+            name="Space suit"
+          />
+          <Item
+            isPacked={true}
+            name="Helmet with a golden leaf"
+          />
+          <Item
+            isPacked={false}
+            name="Photo of Tam"
+          />
+        </ul>
+      </section>
+    );
+  }
